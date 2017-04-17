63cf518d058123ecd2fabcb06dab04aae9905d84
Rimid Volva <Rimid.Volva@outlook.com>
2017-04-17 11:54:50 -0400

    reorg structure, removed old files

--- CAVA/cava_util.ps1
+++ CAVA/cava_util.ps1
@@ -1,5 +1,17 @@
 # CAVA CLASS
 
+# May need to register CAVA WMI class for this script to work:
+#
+# On each CAVA server:
+#
+# Open command window as Administrator and execute the following commands:
+# cd %SystemRoot%\system32\wbem\mof 
+# notepad cava.mof
+#		Add as the first line in the file (incuding the # sign before PRAGMA)
+#		#PRAGMA AUTORECOVER
+# 		save cava.mof
+# mofcomp cava.mof 
+
 # AVEngineState     : Up
 # AVEngineType      : Network Associates
 # FilesScanned      : 2204761
@@ -11,59 +23,55 @@
 # State             : NORMAL
 # Version           : 6.5.0.0
 
-param ( [int]$SampleInterval, 
-	[int]$Count, 
-	[ValidateSet("SaturationPercent","ScansPerSec")]$Metric, 
-	[Parameter(Mandatory=$True)] [string[]]$ComputerName  
+param ( 
+	[Parameter(Mandatory=$True)][string[]]$ComputerName,
+	[int]$SampleInterval, 
+	[int]$Count
+
 )
 
 $DefaultSampleInterval=10
 $DefaultCount=10000
-$DefaultMetric="SaturationPercent"
+#$DefaultMetric="SaturationPercent"
 
 if( ! $SampleInterval ) { $SampleInterval=$DefaultSampleInterval }
 if( ! $Count ) { $Count=$DefaultCount }
-if( ! $Metric ) { $Metric=$DefaultMetric }
 
 "CAVA Utilization Report"
 "-----------------------"
-"SampleInterval:	$SampleInterval"
-"Count:		$count"
-"Metric:		$Metric"
-"CAVA Servers: 	$ComputerName"
+"SampleInterval (sec):	$SampleInterval"
+"Count:			$count"
+"CAVA Servers: 		$ComputerName"
 ""
 
-$HeadRow="{0,-10} {1,-8} " -f "Date","Time"
+$HeadRow="{0,-10} {1,-8} " -f "Date","Time" 
+$SecondRow="{0,10} {1,8} " -f "----------","--------" 
 
 foreach( $Server in $ComputerName ) {
-	$HeadRow+="{0,15} " -f $Server
+	$HeadRow+="| {0,14} |  " -f $Server
+	$SecondRow+="Scans    Ms   Busy  " 
+
 }
 
-$HeadRow+="{0,7}" -f "Average"
-
 $HeadRow
+$SecondRow
 
 
 function AVUtilReport {
 	for( $i=0; $i -lt $Count; $i++ ) {
 
 		$OutputLine=$(get-date -uformat "%m-%d-%Y %H:%M:%S") + " "
-		$Average=0
-		$ServerCount=$ComputerName.count
 
 		foreach( $CAVAServer in $ComputerName ) {
 			$cava=gwmi -ComputerName $CAVAServer -namespace root\emc -class CAVA 
 
 			if( $cava.State -match "NORMAL" ) {
-				$OutputLine += "{0,15:N1} " -f $cava.$Metric
-				$Average+=$cava.$Metric
+				$SaturationPercent=$cava.SaturationPercent*100
+				$OutputLine += "{0,5:N1} {1,5:N0} {2,5:N1}%  " -f $cava.ScansPerSec,$cava.MilliSecsPerScan,$SaturationPercent
 			} else {
-				$OutputLine += "{0,15} " -f $cava.State
-				$ServerCount--
+				$OutputLine += "{0,18}  " -f $cava.State
 			}
 		}
-		$Average=$Average/$ServerCount
-		$OutputLine += "{0,7:N1}" -f $Average
 		$OutputLine 
 
 		sleep $SampleInterval
--- VNX/runvnxcmd.ksh
+++
@@ -1,5 +0,0 @@
-cat vnxlist.txt | while read sn ip model site; 
-do 
-	echo $sn
-	echo naviseccli -h $ip CMD
-done
